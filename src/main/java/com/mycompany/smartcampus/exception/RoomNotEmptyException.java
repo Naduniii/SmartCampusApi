690f31b05713f1517a3d47b07d53c511f52cc31f
Naduniii <naduniameesha17141@gmail.com>
2026-04-21 17:38:44 +0530

    Create 409 Conflict mapper for RoomNotEmptyException

---
+++ src/main/java/com/mycompany/smartcampus/exception/RoomNotEmptyException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.smartcampus.exception;
+
+/**
+ *
+ * @author naduniameesha
+ */
+
+public class RoomNotEmptyException extends RuntimeException {
+    public RoomNotEmptyException(String errorMsg) {
+        super(errorMsg);
+    }
+}
