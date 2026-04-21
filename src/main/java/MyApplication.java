b3095317414d822c0edba79b1dc7f95fc108357a
Naduniii <naduniameesha17141@gmail.com>
2026-04-21 18:04:43 +0530

    Register all ExceptionMappers & loggingFilter in MyApplication configuration

--- src/main/java/MyApplication.java
+++ src/main/java/MyApplication.java
@@ -1,4 +1,12 @@
 
+import com.mycompany.smartcampus.exception.GlobalExceptionMapper;
+import com.mycompany.smartcampus.exception.JsonParseExceptionMapper;
+import com.mycompany.smartcampus.exception.LinkedResourceNotFoundExceptionMapper;
+import com.mycompany.smartcampus.exception.NotFoundExceptionMapper;
+import com.mycompany.smartcampus.exception.ResourceNotFoundExceptionMapper;
+import com.mycompany.smartcampus.exception.RoomNotEmptyExceptionMapper;
+import com.mycompany.smartcampus.exception.SensorUnavailableExceptionMapper;
+import com.mycompany.smartcampus.filter.LoggingFilter;
 import com.mycompany.smartcampusapi.resources.DiscoveryResource;
 import com.mycompany.smartcampusapi.resources.SensorResource;
 import com.mycompany.smartcampusapi.resources.SensorRoomResource;
@@ -27,10 +35,16 @@ public class MyApplication extends Application {
         registeredClasses.add(DiscoveryResource.class);
         registeredClasses.add(SensorRoomResource.class);
         registeredClasses.add(SensorResource.class);
+        registeredClasses.add(RoomNotEmptyExceptionMapper.class);
+        registeredClasses.add(LinkedResourceNotFoundExceptionMapper.class);
+        registeredClasses.add(SensorUnavailableExceptionMapper.class);
+        registeredClasses.add(GlobalExceptionMapper.class);
+        registeredClasses.add(LoggingFilter.class);
+        registeredClasses.add(JsonParseExceptionMapper.class);
+        registeredClasses.add(ResourceNotFoundExceptionMapper.class);
+        registeredClasses.add(NotFoundExceptionMapper.class);
         
-    
         return registeredClasses;
-        
-    
     }
 }
+
